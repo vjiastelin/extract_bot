959242651f7d10f74966b87553f6ee152b8b05ef
vjiastelin <vjiastelin@gmail.com>
2022-11-19 02:35:31 +0500

    add 100 to price

--- scripts/utils.py
+++ scripts/utils.py
@@ -170,7 +170,7 @@ def buid_dataframes(tables,df_dict,regular_expression,price_date,currency):
     return df
 
 def create_pdf(df: pd.DataFrame, pdf_file_path: str, price_date: datetime, currency):
-    df['price_usd'] = (df['price_usd'] * 1.1).astype(int)
+    df['price_usd'] = (df['price_usd'] * 1.1 + 100).astype(int)
     df.rename(
             {'asic_name_raw': 'Наименование','price_usd': 'Цена (USDT)'}, axis=1, inplace=True)
     style_list = ["bootstrap.min.css","report.css"]
